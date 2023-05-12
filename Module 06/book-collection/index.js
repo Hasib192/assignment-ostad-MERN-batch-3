6856050762cbe5ae8cfdf7f5ad14894a5e6c506f
Hasib <hasibhosen7612@gmail.com>
2023-05-12 22:35:31 +0600

    Assignment of Module 06 completed

--- Module 06/book-collection/index.js
+++ Module 06/book-collection/index.js
@@ -22,15 +22,29 @@ app.post("/books", (req, res) => {
   let { title, author, publishedDate } = req.body;
 
   if (!title || !author) {
-    res.status(400).send("Title and author are required.");
+    res.status(400).end("Title and author are required.");
+  } else {
+    let id = uuidv4();
+
+    let newBook = { id, title, author, publishedDate };
+
+    books.push(newBook);
+    res.status(201).end();
   }
+});
 
-  let id = uuidv4();
+app.delete("/books/:id", (req, res) => {
+  let bookID = req.params.id;
+  let bookIndex = books.findIndex((book) => book.id == bookID);
 
-  let newBook = { id, title, author, publishedDate };
-
-  books.push(newBook);
-  res.status(201).end();
+  if (bookIndex !== -1) {
+    books.splice(bookIndex, 1);
+    res.json({ msg: "Book deleted successfully" });
+  } else {
+    res.status(404).json({ msg: "Books not found" });
+  }
+  res.json(books);
+  res.end();
 });
 
 app.listen(PORT, () => {
