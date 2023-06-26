9085d3cc3c2a9fa4c1c4141598fabe7f0090e155
Hasib <hasibhosen7612@gmail.com>
2023-06-26 16:11:13 +0600

    completed assignment

--- Module 13/README.md
+++ Module 13/README.md
@@ -11,14 +11,3 @@ Live Link: https://bootstrap-web-application-assignment.netlify.app/
 **Given Task**
 
 Build a web application using Bootstrap version 5.The application should include various components and features related to Bootstrap's concepts such as light/dark mode, container, row, column, responsive layout, gutters, typography, images, table, form, alert, button, list group and spinner, progress bar, placeholders, pagination, canvas, modal, tab view, navbar, navbar positioning, aspect ratio, and utilities.
-
-**Completed**
-
-Built a web application to practice Bootstrap version 5.
-
-So far, This application contains
-
-1. Navbar
-2. Light/Dark mode
-3. Table
-4. Form
--- Module 13/js/main.js
+++ Module 13/js/main.js
@@ -6,19 +6,21 @@ modes.forEach((mode) => {
     const theme = html[0].getAttribute("data-bs-theme");
 
     if (theme === "light") {
-      document.getElementById("logo").src = "img/logo-white.png";
     }
   });
 });
 
-// // switch mode
-// mode.addEventListener("click", () => {
-//   const mode = html[0].getAttribute("data-bs-theme");
-//   if (mode === "light") {
-//     html[0].setAttribute("data-bs-theme", "dark");
-//     header.style.background = "#000";
-//   } else {
-//     html[0].setAttribute("data-bs-theme", "light");
-//     header.style.background = "#edf2fc";
-//   }
-// });
+const alertPlaceholder = document.getElementById("liveAlertPlaceholder");
+const appendAlert = (message, type) => {
+  const wrapper = document.createElement("div");
+  wrapper.innerHTML = [`<div class="alert alert-${type} alert-dismissible" role="alert">`, `   <div>${message}</div>`, '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>', "</div>"].join("");
+
+  alertPlaceholder.append(wrapper);
+};
+
+const alertTrigger = document.getElementById("liveAlertBtn");
+if (alertTrigger) {
+  alertTrigger.addEventListener("click", () => {
+    appendAlert("Nice, you triggered this alert message!", "success");
+  });
+}
